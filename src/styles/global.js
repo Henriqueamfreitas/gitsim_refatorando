63880235f18f74ff3a1fec2553e45fbf129142ff
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-05-09 18:39:38 -0300

    global, reset e typography

---
+++ src/styles/global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"
+
+export const Global = createGlobalStyle`
+    :root{
+        --color-grey5: #000000;
+        --color-grey4: #121214;
+        --color-grey3: #212529;
+        --color-grey2: #343B41;
+        --color-grey1: #868E96;
+        --color-grey0: #F8F9FA;
+        --color-grey: #FFFFFF;
+    }
+`
